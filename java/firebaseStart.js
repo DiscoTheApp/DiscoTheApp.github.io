b4c9212e881c90ceaf1fedc0d2eff1f522d07969
DiscoTheApp <Tushar@iMac.local>
2017-03-08 15:03:33 -0800

    Import Base
    Base Website

---
+++ java/firebaseStart.js
@@ -0,0 +1,10 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDquIdmKQfr-QGF3kyEb28FzAboOPBE36g",
+    authDomain: "disco-6a3bf.firebaseapp.com",
+    databaseURL: "https://disco-6a3bf.firebaseio.com",
+    storageBucket: "disco-6a3bf.appspot.com",
+    messagingSenderId: "71660981931"
+  };
+  firebase.initializeApp(config);
